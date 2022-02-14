52e07d5e1445bc580c90d73de0fe5e374d12d91c
Charlie Laabs <charlielaabs@gmail.com>
2022-02-13 20:45:23 -0600

    Parse demux nexted payloads

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,35 @@
 import protobuf from 'protobufjs';
 import glob from 'glob';
 import { readFileSync, outputJSONSync } from 'fs-extra';
-import * as rawproto from 'rawproto';
 import jsonHandler from './util/json-dupe-parse'; // Handles duplicate JSON keys
+import { ServiceRequestUpstream, ServiceResponseDownstream } from './types/demux';
+
+const DEMUX_HOST = 'dmx.upc.ubisoft.com';
 
 const protoFiles = glob.sync('proto/**/*.proto');
 console.log(`Loaded ${protoFiles.length} protos`);
 const packageDefinition = protobuf.loadSync(protoFiles);
-outputJSONSync('definition.json', packageDefinition.toJSON());
+
+const demux = packageDefinition.lookup('mg.protocol.demux') as protobuf.Namespace;
+
+const serviceMap: Record<string, protobuf.Namespace> = {
+  utility_service: packageDefinition.lookup('mg.protocol.utility') as protobuf.Namespace,
+  ownership_service: packageDefinition.lookup('mg.protocol.ownership') as protobuf.Namespace,
+  denuvo_service: packageDefinition.lookup('mg.protocol.denuvo_service') as protobuf.Namespace,
+  store_service: packageDefinition.lookup('mg.protocol.store') as protobuf.Namespace,
+  friends_service: packageDefinition.lookup('mg.protocol.friends') as protobuf.Namespace,
+  playtime_service: packageDefinition.lookup('mg.playtime') as protobuf.Namespace,
+  party_service: packageDefinition.lookup('mg.protocol.party') as protobuf.Namespace,
+  download_service: packageDefinition.lookup('mg.protocol.download') as protobuf.Namespace,
+  client_configuration_service: packageDefinition.lookup(
+    'mg.protocol.client_configuration'
+  ) as protobuf.Namespace,
+};
 
 interface TLSPayload {
   data: Buffer;
-  timestamp: Date;
-  src: string;
-  dst: string;
+  frame: number;
+  direction: 'Upstream' | 'Downstream';
 }
 const jsonBufferReplacer = (_key: string, val: any) => {
   if (val?.type === 'Buffer') {
@@ -29,11 +45,16 @@ const getTLSPayload = (wsPacket: any): TLSPayload[] | null => {
   const layers = wsPacket._source?.layers;
   const data: string = layers?.data?.['data.data'];
   if (!data) return null;
-  const timestamp = new Date(
-    parseFloat(wsPacket._source?.layers?.frame['frame.time_epoch']) * 1000
-  );
-  const src = wsPacket._source?.layers?.ip['ip.src'];
-  const dst = wsPacket._source?.layers?.ip['ip.dst'];
+  const frame = parseInt(layers.frame['frame.number'], 10);
+
+  let direction: 'Upstream' | 'Downstream' | null = null;
+  if (wsPacket._source?.layers?.ip['ip.dst_host'] === DEMUX_HOST) {
+    direction = 'Upstream';
+  }
+  if (wsPacket._source?.layers?.ip['ip.src_host'] === DEMUX_HOST) {
+    direction = 'Downstream';
+  }
+  if (!direction) return null;
 
   const dataKeys = Object.keys(layers).filter((key) => key.match(/data\d*/));
   const payloads = dataKeys
@@ -41,9 +62,8 @@ const getTLSPayload = (wsPacket: any): TLSPayload[] | null => {
       const currentData = layers[key]?.['data.data'];
       if (!currentData) return null;
       return {
-        timestamp,
-        src,
-        dst,
+        frame,
+        direction,
         data: Buffer.from(currentData.replace(/:/g, ''), 'hex'),
       };
     })
@@ -81,22 +101,36 @@ const payloadJoiner = (payloads: TLSPayload[]): TLSPayload[] => {
   return joinedPayloads;
 };
 
-const recurseDefinitions = (
-  nodes: (protobuf.Namespace | protobuf.Type | protobuf.Service | protobuf.ReflectionObject)[]
-): protobuf.Type[] => {
-  const types: protobuf.Type[] = nodes
-    .map((node) => {
-      if (node instanceof protobuf.Type) {
-        return node;
-      }
-      if (node instanceof protobuf.Namespace) {
-        return recurseDefinitions(node.nestedArray);
-      }
-      return null;
-    })
-    .filter((n): n is protobuf.Type[] => n !== null)
-    .flat();
-  return types;
+const decodeRequests = (payloads: TLSPayload[]): any[] => {
+  const pendingRequests: Record<number, string> = {};
+  const decodedDemux = payloads.map((payload) => {
+    const schema = demux.lookupType(payload.direction);
+    const body = schema.decode(payload.data) as any;
+    if (body?.request?.serviceRequest) {
+      const svcReqBody: ServiceRequestUpstream = body;
+      const { requestId } = svcReqBody.request;
+      const { data, service } = svcReqBody.request.serviceRequest;
+      const serviceSchema = serviceMap[service];
+      if (!serviceSchema) throw new Error(`Missing service: ${service}`);
+      const dataType = serviceSchema.lookupType(payload.direction);
+      const decodedData = dataType.decode(data);
+      pendingRequests[requestId] = service;
+      body.request.serviceRequest.data = decodedData;
+    }
+    if (body?.response?.serviceRsp) {
+      const svcRspBody: ServiceResponseDownstream = body;
+      const { requestId } = svcRspBody.response;
+      const { data } = svcRspBody.response.serviceRsp;
+      const serviceName = pendingRequests[requestId];
+      const serviceSchema = serviceMap[serviceName];
+      const dataType = serviceSchema.lookupType(payload.direction);
+      const decodedData = dataType.decode(data);
+      delete pendingRequests[requestId];
+      body.response.serviceRsp.data = decodedData;
+    }
+    return body;
+  });
+  return decodedDemux;
 };
 
 const main = () => {
@@ -109,33 +143,10 @@ const main = () => {
   console.log(`${payloads.length} payloads found`);
   const joinedPayloads = payloadJoiner(payloads);
   outputJSONSync('tls-payloads.json', joinedPayloads, { spaces: 2 });
-  const rawDecodedPayloads = joinedPayloads.map((p) => {
-    const d = rawproto.getData(p.data);
-    return {
-      ...p,
-      data: d,
-    };
-  });
-  outputJSONSync('decoded-payloads.json', rawDecodedPayloads, {
-    spaces: 2,
-    replacer: jsonBufferReplacer,
-  });
 
-  const types = recurseDefinitions(packageDefinition.nestedArray);
-  console.log(`resolved ${types.length} proto types`);
-  const responses = joinedPayloads.flatMap((p) => {
-    return types
-      .map((type) => {
-        try {
-          return type.decode(p.data);
-        } catch (err) {
-          return null;
-        }
-      })
-      .filter((d): d is protobuf.Message<any> => d !== null);
-  });
-  console.log(`Generated ${responses.length} responses`);
-  outputJSONSync('decodes.json', responses, {
+  const decodedDemuxes = decodeRequests(joinedPayloads);
+  console.log(`Generated ${decodedDemuxes.length} responses`);
+  outputJSONSync('decodes.json', decodedDemuxes, {
     spaces: 2,
     replacer: jsonBufferReplacer,
   });
